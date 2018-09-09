9b4072f4a00a9bba5bbcd2c99f44d000d20fc3ef
dgmartinezg <dgmartinezg@unal.edu.co>
2018-09-09 17:13:55 -0500

    version final

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,10 +1,14 @@
 from turtle import *
 import math as m
-def poli(lado,n,p):
-    for i in range(p):
-            penup()
-            goto(100*m.cos((90+(360/p)*i)*(m.pi/180)),100*m.sin((90+(360/p)*i)*(m.pi/180)))
-            pendown()
-            for j in range(n):
-                forward(lado)
-                left(360/n)
+import msvcrt
+lado=30
+n=int(input("introdusca el numero de lados del poligono de las puntas"))
+p=int(input("introdusca el numero de lados del poligo grande"))
+for i in range(p):
+	penup()
+	goto(100*m.cos((90+(360/p)*i)*(m.pi/180)),100*m.sin((90+(360/p)*i)*(m.pi/180)))
+	pendown()
+	for j in range(n):
+		forward(lado)
+		left(360/n)
+msvcrt.getch()
