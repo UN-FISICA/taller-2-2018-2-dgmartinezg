7c5119fdb112d7ce60f9f8f1d7c931a0a9503654
dgmartinezg <dgmartinezg@unal.edu.co>
2018-09-09 17:50:53 -0500

    Ejercicio4, version final

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 from turtle import *
+import msvcrt
 import math as m
-def pira(n,p,lado): #n el numero la base , lado es el largo del lado del poligono
-    d=0             #p numero de lados del poligono
-    for j in range(n): #este for controla la altura de la piramide
-        penup()
-        goto(d*lado,d*lado+d)
-        pendown()
-        for i in range(n): #este for controla la repeticion en cada fila de la piramide
-            penup()
-            forward(lado*2)
-            pendown()
-            for k in range(p): #este for da forma a cada figura de la piramide
-                forward(lado)
-                left(360/p)
-            
-            print(i)  #imprime el numero de poligonos en cada fila
-            
-        n=n-1
-        d=d+1
-        
-        print("n= ",n) #imprime para revisar como cambian las variables 
-        print("d= ",d)
-
+n= int(input("introdusca el numero de la base de la piramide:  "))#n el numero de la base de la piramide
+p= int(input("introdusca el numero de lados de los poligonos en la piramide:  ")) #p numero de lados del poligono
+lado=15
+d=0             #p numero de lados del poligono
+for j in range(n): #este for controla la altura de la piramide
+	penup()
+	goto(2*d*lado,2*d*lado+2*d)
+	pendown()
+	for i in range(n): #este for controla la repeticion en cada fila de la piramide
+		penup()
+		forward(lado*4)
+		pendown()
+		for k in range(p): #este for da forma a cada figura de la piramide
+			forward(lado)
+			left(360/p)            
+	n=n-1
+	d=d+1
+	print("n= ",n) #imprime para revisar como cambian las variables 
+	print("d= ",d)
+msvcrt.getch()
     
 
 
