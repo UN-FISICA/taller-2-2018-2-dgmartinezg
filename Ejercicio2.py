01efb5048efd8284355fd0049a8dc5b36e436861
dgmartinezg <dgmartinezg@unal.edu.co>
2018-09-09 17:03:56 -0500

    Forma final del Ejercicio2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,13 +1,14 @@
 from turtle import *
+import msvcrt
 import math as m
-def poli(lado,n):
-    for i in range(4):
-            penup()
-            goto(100*m.cos((135+90*i)*(m.pi/180)),100*m.sin((135+90*i)*(m.pi/180)))
-            pendown()
-            for j in range(n):
-                forward(lado)
-                left(360/n)
-                
-                 
+lado=30
+n = int(input("introdusca el numero de lados del poligono en las equinas"))
+for i in range(4):
+	penup()
+	goto(100*m.cos((135+90*i)*(m.pi/180)),100*m.sin((135+90*i)*(m.pi/180)))
+	pendown()
+	for j in range(n):
+		forward(lado)
+		left(360/n)
+msvcrt.getch()                        
 
