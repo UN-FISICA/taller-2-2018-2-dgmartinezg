8f95fac0dd9676d765392be5eb65bce42ba7c2bb
dgmartinezg <dgmartinezg@unal.edu.co>
2018-09-09 16:42:11 -0500

    esta es la forma final, cumple el objetivo

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,10 +1,12 @@
 from turtle import *
-def cuadrado(lado):
-	for i in range(4):
-		penup()
-		forward(100)
-		pendown()
-		for j in range(5):
-			forward(lado)
-			right(90)
-			
+import msvcrt
+lado=40
+for i in range(4):
+	penup()
+	forward(100)
+	pendown()
+	for j in range(5):
+		forward(lado)
+		right(90)
+msvcrt.getch()
+
